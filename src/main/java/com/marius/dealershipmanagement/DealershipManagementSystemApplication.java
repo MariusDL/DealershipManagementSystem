53b0c502c3fedae9d23bfc2d16b4be2e7d01e304
MariusDL <mariusdl@protonmail.com>
2021-05-21 19:23:02 +0100

    Initial commit

---
+++ src/main/java/com/marius/dealershipmanagement/DealershipManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.marius.dealershipmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealershipManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealershipManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marius/dealershipmanagement/DealershipManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marius.dealershipmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealershipManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
